d74ba8f950e91cb68497a849c2d5f6e1d66c70e1
Truong Kim <truongkim@b13technology.com>
2021-05-02 04:20:05 +0700

    search 1 slang word, 2 search definition

---
+++ SlangWord.java
@@ -0,0 +1,147 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.io.Serializable;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.*;
+
+class SlangWord implements Serializable {
+    String id;
+    String des;
+    static List<SlangWord> importCSV(String filename) throws IOException {
+        List<SlangWord> dsSlangWord = new ArrayList<>();
+        String path = filename;
+
+        try (BufferedReader bir = new BufferedReader(new FileReader(path))) {
+            String line = bir.readLine();
+            while (line != null) {
+                try{
+                    String[] result = line.split("`");
+                    SlangWord sl = new SlangWord();
+                    sl.id = result[0];
+                    sl.des = result[1];
+                    dsSlangWord.add(sl);
+                    line = bir.readLine();
+                }
+                catch(Exception e){
+                    SlangWord sl = new SlangWord();
+                    sl.id = line;
+                    sl.des = line;
+                    dsSlangWord.add(sl);
+                    line = bir.readLine();
+                }
+               
+        }
+        return dsSlangWord;
+        }
+    }
+    static String timKiemTheoSlangWord(List<SlangWord> dsSlangWord)throws IOException  {
+        SlangWord sw = new SlangWord();
+        try {
+            BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Nhap slang word: ");
+            sw.id = dataIn.readLine();
+            for(int i=0;i<dsSlangWord.size();i++){
+                if(dsSlangWord.get(i).id == null ? sw.id == null : dsSlangWord.get(i).id.equals(sw.id))
+                {
+                    System.out.println("Result: "+dsSlangWord.get(i).des);
+                    return dsSlangWord.get(i).des;
+                }
+            }
+            System.out.println("Not found");
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+    }
+    static String timKiemTheoDefintion(List<SlangWord> dsSlangWord)throws IOException  {
+        SlangWord sw = new SlangWord();
+        List<String> dsSlangWordResult = new ArrayList<>();
+        try {
+            BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Nhap defintion: ");
+            sw.des = dataIn.readLine();
+            System.out.println("Result:");
+            for(int i=0;i<dsSlangWord.size();i++){
+                if(dsSlangWord.get(i).des.contains(sw.des))
+                {
+                    System.out.println(dsSlangWord.get(i).id+" - "+dsSlangWord.get(i).des);
+                    dsSlangWordResult.add(dsSlangWord.get(i).id);
+                }
+            }
+            if(dsSlangWordResult.size()==0)
+                System.out.print("Not found");
+            System.out.println(" ");
+            return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+
+    }
+
+
+    public static void main(String args[]) throws Exception {
+        // CopyRight @ Kim Nhut Truong - 20424083 - 20HCB2
+
+        String inputFile = "slang.txt";
+//        writeListSlangWordBinary();
+
+        List<SlangWord> dsSlangWord = new ArrayList<>();
+        dsSlangWord= importCSV(inputFile);
+        timKiemTheoSlangWord(dsSlangWord);
+        timKiemTheoDefintion(dsSlangWord);
+//        dsSlangWord = readListSlangWordBinary();
+//        dsSlangWord = deleteSlangWord(dsSlangWord);
+//        dsSlangWord = addSlangWord(dsSlangWord);
+//        dsSlangWord = capNhatHS(dsSlangWord);
+//        dsSlangWord = sortByDiem(dsSlangWord, true);
+//        dsSlangWord = sortByDiem(dsSlangWord, false);
+//        dsSlangWord = sortByMHS(dsSlangWord, true);
+//        dsSlangWord = sortByMHS(dsSlangWord, false);
+//        exportCSV(dsSlangWord);
+//        dsSlangWord = importCSV();
+
+//        System.out.println(dsSlangWord.get(7).TenHS);
+//        System.out.println(dsSlangWord.size());
+//        SlangWord hs = new SlangWord();
+//        hs.MHS = "MHS";
+//        hs.TenHS = "TenHS";
+//        hs.Diem = "1";
+//        hs.HinhAnh = "HinhAnh";
+//        hs.DiaChi = "DiaChi";
+//        hs.GhiChu = "GhiChu";
+        // hs = capNhatHS(hs);
+
+    }
+}
